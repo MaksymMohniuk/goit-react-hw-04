92161ec9c42d2fdd685ad93ee7a790183b8c9452
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-04-01 00:26:32 +0300

    http-request-started

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 // import { useState } from 'react'
 
-import './App.css'
+import "./App.css";
 
-function App() {
-  
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-  return (
-    <>
-      
-    </>
-  )
-}
+const App = () => {
+  const [photos, setPhotos] = useState(null);
 
-export default App
+  useEffect(() => {
+    async function fetchPhotos() {
+      const { data } = await axios.get(
+        "https://api.unsplash.com/photos/?client_id=oGYzQqArk2YQGtYqQNUjoqd5R_WjC4bentc-JV8nvfk"
+      );
+      console.log(data);
+      setPhotos(data);
+    }
+    fetchPhotos();
+  }, []);
+
+  return <div>AppWithHTTPRequests</div>;
+};
+
+export default App;
