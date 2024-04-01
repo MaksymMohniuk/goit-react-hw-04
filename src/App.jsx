ea51a78b23dd5f4d9d37405354b5ac12a666f596
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-04-01 23:52:22 +0300

    added-render-loader&spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,68 @@
-// import { useState } from 'react'
-
 import "./App.css";
-
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { MutatingDots } from "react-loader-spinner";
 
 const App = () => {
   const [photos, setPhotos] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { data } = await axios.get(
-        "https://api.unsplash.com/photos/?client_id=oGYzQqArk2YQGtYqQNUjoqd5R_WjC4bentc-JV8nvfk"
-      );
-      console.log(data);
-      setPhotos(data);
+      try {
+        setIsLoading(true);
+        const { data } = await axios.get(
+          "https://api.unsplash.com/photos/?client_id=oGYzQqArk2YQGtYqQNUjoqd5R_WjC4bentc-JV8nvfk"
+        );
+        setPhotos(data);
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
     }
     fetchPhotos();
   }, []);
 
-  return <div>AppWithHTTPRequests</div>;
+  return (
+    <>
+      {isLoading && (
+        <div>
+          <MutatingDots
+            visible={true}
+            height="100"
+            width="100"
+            color="#4fa94d"
+            secondaryColor="#4fa94d"
+            radius="12.5"
+            ariaLabel="mutating-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      )}
+      {isError && <p>Oops, something went wrong!</p>}
+      <ul>
+        {Array.isArray(photos) &&
+          photos.map((photo) => {
+            return (
+              <li key={photo.id}>
+                <img
+                  width={250}
+                  src={photo.urls.small}
+                  alt={photo.alt_description}
+                />
+                <h2>Description: {photo.description}</h2>
+                <p>
+                  Likes: <b>{photo.likes}</b>
+                </p>
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 };
 
 export default App;
